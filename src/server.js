a1b32201734e937ddfa286b54e3021c5a2fe824a
Christopher Enea <cenea@ltu.edu>
2021-02-06 21:12:42 -0500

    Added functionality to server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+// create the app
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+// changed port to 8080
 app.listen(8080);
